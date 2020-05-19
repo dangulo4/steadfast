dbdd6ac5c08694a02146a9ef49f2db423049a986
Alexia Chalita <chalitaalexia@gmail.com>
2020-05-18 20:05:32 -0700

    added header

---
+++ containers/header.js/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const header = () => {
+return <Jumbotron>
+<h1>Steadfast</h1>
+<p>
+  Find contacts and leads fast. Get back to business.
+</p>
+<p>
+  <Button variant="primary">Get Started</Button>
+</p>
+</Jumbotron>
+
+};
+
+  export default header;
