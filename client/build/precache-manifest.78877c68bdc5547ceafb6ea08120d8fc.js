2b6e1a2823f7f4bcca74f0500b861fa8dd38c102
Matthew Bell <matthewbell1030@gmail.com>
2020-05-28 00:30:16 -0700

    completed fixing errors foud when running npm i in root folder

---
+++ client/build/precache-manifest.78877c68bdc5547ceafb6ea08120d8fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0bc45f4789b440c31f6",
+    "url": "/static/css/main.e4ae63d4.chunk.css"
+  },
+  {
+    "revision": "f0bc45f4789b440c31f6",
+    "url": "/static/js/main.a578722b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3d98bd4d83813e3bc90d",
+    "url": "/static/js/2.ba142190.chunk.js"
+  },
+  {
+    "revision": "0fbe940792655b5b1894994e7519718d",
+    "url": "/index.html"
+  }
+];
--- client/src/components/Auth/Logout.js
+++ client/src/components/Auth/Logout.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from "react";
+import propTypes from "prop-types";
 import { NavLink } from "reactstrap";
 import { connect } from "react-redux";
 import { logout } from "../../actions/authActions";
 
 export class Logout extends Component {
     //Static PropTypes
-    static PropTypes = {
-        logout: this.PropTypes.func.isRequired
+    static propTypes = {
+        logout: this.propTypes.func.isRequired
     };
 
     
