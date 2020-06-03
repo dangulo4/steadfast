c66169ff4ead572f38176832b740490db4a82405
Daniel Angulo <dangulo4@gmail.com>
2020-06-02 20:58:59 -0700

    npm run build

---
+++ client/build/precache-manifest.7b61972448bf1d6a6818d71a75a95d5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3dee8e78349b1f832f",
+    "url": "/static/css/main.4eb378b4.chunk.css"
+  },
+  {
+    "revision": "2c3dee8e78349b1f832f",
+    "url": "/static/js/main.c9fda18c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1d7bb9064ad54216f32f",
+    "url": "/static/js/2.682f31d8.chunk.js"
+  },
+  {
+    "revision": "72d2b265cc210038bfb3cb6b6902b12b",
+    "url": "/index.html"
+  }
+];
