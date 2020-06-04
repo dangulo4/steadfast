c24e0ec8935e61300c8c6742114601d1d810321b
Matthew Bell <matthewbell1030@gmail.com>
2020-06-04 16:55:35 -0700

    corrected import issues, and added constructor to /navbar/index.js.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,75 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Login from '../Login';
-import Signup from '../Signup';
+import Modal from 'react-responsive-modal';
+import Login from '../Modals/Login';
+import Signup from '../Modals/Signup';
 
 
-function Navbar() {
-  const { login, signup } = this.state;
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        SteadFast
-      </Link>
-      <div>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/"
-              className={
-                window.location.pathname === '/' ||
-                window.location.pathname === '/about'
-                  ? 'nav-link active'
-                  : 'nav-link'
-              }
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contacts"
-              className={
-                window.location.pathname === '/contacts'
-                  ? 'nav-link active'
-                  : 'nav-link'
-              }
-            >
-              Contact
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/search"
-              className={
-                window.location.pathname === '/search'
-                  ? 'nav-link active'
-                  : 'nav-link'
-              }
-            >
-              Search
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <button
-              className = 'btn btn-secondary'
-              id = 'login'
-              onClick = {this.onLoginModalOpen}>
-                Login
-            </button>
-          </li>
-          <li className='nav-item'>
-            <button
-              className = 'btn btn-secondary'
-              id = 'signup'
-              onClick = {this.onSignupModalOpen}>
-                Signup
-            </button>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+class Navbar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      login: false,
+      signup: false
+    }
+}
+  render() {
+    const { login, signup } = this.state;  
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          SteadFast
+        </Link>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  window.location.pathname === '/' ||
+                  window.location.pathname === '/about'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contacts"
+                className={
+                  window.location.pathname === '/contacts'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/search"
+                className={
+                  window.location.pathname === '/search'
+                    ? 'nav-link active'
+                    : 'nav-link'
+                }
+              >
+                Search
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <button
+                className = 'btn btn-secondary'
+                id = 'login'
+                onClick = {this.onLoginModalOpen}>
+                  Login
+              </button>
+            </li>
+            <li className='nav-item'>
+              <button
+                className = 'btn btn-secondary'
+                id = 'signup'
+                onClick = {this.onSignupModalOpen}>
+                  Signup
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
