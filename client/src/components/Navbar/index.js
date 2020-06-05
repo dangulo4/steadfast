32bb1c7d21c7a7ac1dae413af5a2a8c118ac5eb1
Matthew Bell <matthewbell1030@gmail.com>
2020-06-04 17:16:43 -0700

    added modal components to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -15,72 +15,77 @@ class Navbar extends Component {
     }
 }
   render() {
-    const { login, signup } = this.state;  
+    const { login, signup } = this.state;
+
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" to="/">
-          SteadFast
-        </Link>
-        <div>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                to="/"
-                className={
-                  window.location.pathname === '/' ||
-                  window.location.pathname === '/about'
-                    ? 'nav-link active'
-                    : 'nav-link'
-                }
-              >
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/contacts"
-                className={
-                  window.location.pathname === '/contacts'
-                    ? 'nav-link active'
-                    : 'nav-link'
-                }
-              >
-                Contact
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/search"
-                className={
-                  window.location.pathname === '/search'
-                    ? 'nav-link active'
-                    : 'nav-link'
-                }
-              >
-                Search
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <button
-                // style = {{paddingLeft: "20px"}}
-                className = 'btn btn-secondary'
-                id = 'login'
-                onClick = {this.onLoginModalOpen}>
-                  Login
-              </button>
-            </li>
-            <li className='nav-item'>
-              <button
-                // style = {{paddingLeft: "20px"}}
-                className = 'btn btn-secondary'
-                id = 'signup'
-                onClick = {this.onSignupModalOpen}>
-                  Signup
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div>
+        <Login login={this.state.login} ></Login>
+        <Signup signup={this.state.signup} ></Signup>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">
+            SteadFast
+          </Link>
+          <div>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className={
+                    window.location.pathname === '/' ||
+                    window.location.pathname === '/about'
+                      ? 'nav-link active'
+                      : 'nav-link'
+                  }
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/contacts"
+                  className={
+                    window.location.pathname === '/contacts'
+                      ? 'nav-link active'
+                      : 'nav-link'
+                  }
+                >
+                  Contact
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/search"
+                  className={
+                    window.location.pathname === '/search'
+                      ? 'nav-link active'
+                      : 'nav-link'
+                  }
+                >
+                  Search
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <button
+                  // style = {{paddingLeft: "20px"}}
+                  className = 'btn btn-secondary'
+                  id = 'login'
+                  onClick = {this.onLoginModalOpen}>
+                    Login
+                </button>
+              </li>
+              <li className='nav-item'>
+                <button
+                  // style = {{paddingLeft: "20px"}}
+                  className = 'btn btn-secondary'
+                  id = 'signup'
+                  onClick = {this.onSignupModalOpen}>
+                    Signup
+                </button>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     );
   }
 }
